35d8c2086653a17b6423363d8e5e8d594db12ab9
Brian Eddy <beddy@uwf.edu>
2018-10-30 14:45:51 -0500

    removed one of the tests

--- test.js
+++ test.js
@@ -4,8 +4,5 @@ describe('Array', function () {
     it('should return -1 when the value is not present', function () {
       assert.equal([1, 2, 3].indexOf(4), -1);
     });
-    it('should return the index when the value is present', function () {
-      assert.equal([1, 2, 3].indexOf(2), 1);
-    });
   });
 });
